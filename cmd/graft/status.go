09953e94258b817b03f4b7b09e4fb5b21076cf10
Ray Bjorkman <raybjork@gmail.com>
2026-04-03 14:11:36 -0400

    oops

--- cmd/graft/status.go
+++ cmd/graft/status.go
@@ -11,7 +11,7 @@ var statusCmd = &cobra.Command{
 	Use:   "status",
 	Short: "Show connection status overview",
 	RunE: func(cmd *cobra.Command, _ []string) error {
-		client, ctx := newClient(cmd.Context(), false)
+		client, ctx := newClient(cmd.Context(), true)
 		defer client.Close()
 
 		printFn := client.PrintStatus

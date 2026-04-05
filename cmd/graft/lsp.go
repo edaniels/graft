863b0c9e66d091e8ddc63c966d5ede156743985e
Eric Daniels <eric@erdaniels.com>
2026-04-05 16:32:32 -0400

    exp/lsp: better process remote roots and run local lsps when needed (#28)

--- cmd/graft/lsp.go
+++ cmd/graft/lsp.go
@@ -1,13 +1,31 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+
+	graft "github.com/edaniels/graft/pkg"
+)
 
 var lspCmd = &cobra.Command{
 	Use:   "lsp <remote-executable>",
 	Short: "Serve LSP proxy to a remote language server",
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		client, ctx := newClient(cmd.Context(), true)
+		client, ctx, err := graft.NewLocalClient(
+			cmd.Context(),
+			os.Stdout,
+			os.Stderr,
+			func(err error) error {
+				return cliExit(err, 1)
+			},
+			false,
+			logger,
+		)
+		if err != nil {
+			return graft.ExecLocalLSP(args[0])
+		}
 		defer client.Close()
 
 		return client.ServeLSP(ctx, args[0])

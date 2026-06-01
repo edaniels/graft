4d6e614be9f58b07dd7aa67c7ac227edc784f203
Ray Bjorkman <raybjork@gmail.com>
2026-06-01 14:04:46 -0400

    better error

--- pkg/session_manager.go
+++ pkg/session_manager.go
@@ -559,7 +559,14 @@ func (mgr *SessionManager) selectConnection(ctx context.Context, sess *Session, 
 		}
 	}
 
-	return nil, errors.New("failed to find an eligible connection. To use without an explicit connection pin a session with 'graft use'")
+	for connName = range mgr.connMgr.Connections() {
+		break
+	}
+
+	return nil, errors.Errorf(
+		"failed to find an eligible connection; to use without an explicit connection pin with 'graft use %s'",
+		connName,
+	)
 }
 
 // resolveSessionConnection returns the connection for a session, applying the
